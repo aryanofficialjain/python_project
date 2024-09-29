ac42dbfb05b69472342cacfe6913763aedd4d7d9
Aryan Jain <aryanofficialjain@gmail.com>
2024-09-29 16:32:52 +0530

    alomost complete not test yet and nav link and search fucntionality remains incomplete

--- chaicenter/tweet/models.py
+++ chaicenter/tweet/models.py
@@ -2,14 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Tweet (models.Model):
+class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=240)
-    photo = models.ImageField(upload_to='/photos', blank=True, null=True)
-    created_at = models.DateTimeField(auto_created=True)
-    updated_at = models.DateTimeField(auto_created=True)
-
+    photo = models.ImageField(upload_to='photos', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set on creation
+    updated_at = models.DateTimeField(auto_now=True)      # Automatically updated on save
 
     def __str__(self):
         return f"{self.user.username} - {self.text[:10]}"
-
